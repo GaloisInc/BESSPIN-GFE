6f16002b52f27bbd58fdd8208167f0bf4a90f683
Max Waugaman <mwaugaman@galois.com>
2019-01-03 08:35:14 -0800

    Added p1_path option to edit_soc.sh

--- edit_soc.sh
+++ edit_soc.sh
@@ -10,5 +10,7 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 mkdir -p $BASE_DIR/vivado
 cd $BASE_DIR/vivado
 # Run vivado to build a top level project
-vivado -mode batch -source $BASE_DIR/tcl/p1_chisel_soc.tcl -tclargs --origin_dir $BASE_DIR/tcl
+vivado -mode batch -source $BASE_DIR/tcl/p1_soc.tcl \
+-tclargs --origin_dir $BASE_DIR/tcl \
+--p1_path $BASE_DIR/xilinx_chisel_processors/P1/xilinx_ip/p1_normal_jtag_1.0
 # TODO: Consider updating the vivado scripting to use non-project mode
