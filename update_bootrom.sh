3f3d803667296e72abfd797310099c429461c3d9
Max Waugaman <mwaugaman@galois.com>
2019-01-07 15:21:32 -0800

    Added first draft of updatemem script for preloading a rom into bitstream. This needs further parameterization.

---
+++ update_bootrom.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Get the path to the root folder of the git repository
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BITFILE=$BASE_DIR/vivado/p1_chisel_soc/p1_chisel_soc.runs/impl_2/design_1.bit
+LTXFILE=$BASE_DIR/vivado/p1_chisel_soc/p1_chisel_soc.runs/impl_2/design_1.ltx
+
+updatemem -force --meminfo $BASE_DIR/vivado/blk_mem_gen_0.mmi \
+--data $BASE_DIR/bootrom/bootrom.mem \
+--bit $BITFILE --proc dummy -debug \
+-out $BASE_DIR/bitstreams/design_1_bootrom.bit
+cp $LTXFILE $BASE_DIR/bitstreams/design_1_bootrom.ltx
