ff43c79631e96a5bdcc22f9cb708511fb28f7f89
Max Waugaman <mwaugaman@galois.com>
2019-01-09 07:02:18 -0800

    Added some comments and TODO's for updating the bitfile with the desired bootrom.

--- update_bootrom.sh
+++ update_bootrom.sh
@@ -2,8 +2,14 @@
 
 # Get the path to the root folder of the git repository
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+# TODO: Make these paths generic or pass them as arguments
 BITFILE=$BASE_DIR/vivado/p1_chisel_soc/p1_chisel_soc.runs/impl_2/design_1.bit
 LTXFILE=$BASE_DIR/vivado/p1_chisel_soc/p1_chisel_soc.runs/impl_2/design_1.ltx
+
+# TODO: Finish scripting the steps to generate blk_mem_gen_0.mmi
+# For now, open the implemented design in Vivado and run the following:
+# source ../tcl/write_mmi.tcl
+# write_mmi blk_mem_gen_0
 
 mkdir -p $BASE_DIR/vivado
 cd $BASE_DIR/vivado
