b31ff861b0c0b7e3a46a9d3590e4ddeded2090c8
Max Waugaman <mwaugaman@galois.com>
2019-01-14 09:29:44 -0800

    Moved rel_1_testing script to the top level

--- rel_1_test.sh
+++ rel_1_test.sh
@@ -3,16 +3,16 @@
 # Get the path to the script folder of the git repository
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-cd $BASE_DIR
+cd $BASE_DIR/testing/scripts
 
 # Check that it is possible to GDB into the riscv core
 python gdbserver.py ../targets/p1.py ExamineTarget
 
 # Compile and run the UART, BOOTROM, and DDR smoke tests
-cd ../baremetal/asm
+cd $BASE_DIR/testing/baremetal/asm
 make
 
-cd $BASE_DIR
+cd $BASE_DIR/testing/scripts
 python test_gfe_unittest.py
 
 # Optionally, boot FreeRTOS
