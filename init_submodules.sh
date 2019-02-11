160de3c83c5a8c12cb7a89f002a3bf86ddf334dc
Max Waugaman <mwaugaman@galois.com>
2019-02-11 15:30:52 -0800

    Updated init_submodules for FreeRTOS-mirror

--- init_submodules.sh
+++ init_submodules.sh
@@ -5,7 +5,7 @@ home=$(pwd)
 # Initialize the minimum number of submodules necessary to build the project
 # This reduces the runtime of git status and other git commands
 git submodule update --init --recursive riscv-tools \
-FreeRTOS-RISCV bluespec-processors/P1/Piccolo
+FreeRTOS-mirror bluespec-processors/P1/Piccolo
 git submodule update --init chisel_processors
 
 cd chisel_processors
