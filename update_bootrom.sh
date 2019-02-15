cd4e9f466c95e4b84d0a1053c811fd4546cd4be3
Max Waugaman <mwaugaman@galois.com>
2019-02-15 15:19:50 -0800

    Added @00000000 back into generated .mem file. Updatemem appears to prefer having this line.

--- update_bootrom.sh
+++ update_bootrom.sh
@@ -10,6 +10,7 @@ PRJNAME=p1_soc_${p1_name}
 BITFILE=$BASE_DIR/vivado/${PRJNAME}/${PRJNAME}.runs/impl_1/design_1.bit
 LTXFILE=$BASE_DIR/vivado/${PRJNAME}/${PRJNAME}.runs/impl_1/design_1.ltx
 PRJFILE=${PRJNAME}/${PRJNAME}.xpr
+REVMEMFILE=$BASE_DIR/bootrom/bootrom.bytereversed.mem
 
 check_file $BITFILE "Error! Cannot find bit file at $BITFILE"
 check_file $LTXFILE "Error! Cannot find LTX file at $LTXFILE"
@@ -21,9 +22,9 @@ cd $BASE_DIR/vivado
 vivado -mode batch -nojournal -nolog -notrace -source ../tcl/update_bootrom.tcl $PRJFILE
 err_msg $? "Error! Extracting memory information failed!"
 
-echo "Updating memory with data from $BASE_DIR/bootrom/bootrom.bytereversed.mem"
+echo "Updating memory with data from $REVMEMFILE"
 updatemem -force --meminfo $BASE_DIR/vivado/blk_mem_gen_0.mmi \
---data $BASE_DIR/bootrom/bootrom.bytereversed.mem \
+--data $REVMEMFILE \
 --bit $BITFILE --proc dummy -debug \
 -out $BASE_DIR/bitstreams/design_1_bootrom.bit
 err_msg $? "Error! Updating memory failed!"
