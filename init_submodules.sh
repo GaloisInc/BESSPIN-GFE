467fcaba7ac7494a2bbf7ea1fcee13de979dcba2
Max Waugaman <mwaugaman@galois.com>
2019-04-11 14:27:52 -0700

    Handle some edge cases in the init_submodules script. This helps when updating from one release to another

--- init_submodules.sh
+++ init_submodules.sh
@@ -6,8 +6,16 @@ home=$(pwd)
 # This reduces the runtime of git status and other git commands
 git submodule sync
 
+git submodule update --init riscv-tools
+cd riscv-tools
+git submodule sync
+git submodule update --init
+cd riscv-tests
+git submodule sync
+cd ../..
+
 git submodule update --init --recursive riscv-tools \
-FreeRTOS-mirror bluespec-processors/P1/Piccolo  bluespec-processors/P2/Flute busybox riscv-linux bluespec-processors/P3/Tuba
+FreeRTOS-mirror bluespec-processors/P1/Piccolo bluespec-processors/P2/Flute busybox riscv-linux bluespec-processors/P3/Tuba
 git submodule update --init chisel_processors
 
 cd chisel_processors
