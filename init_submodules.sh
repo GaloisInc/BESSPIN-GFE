2be206cd521438e75862c579de918772e38079a6
Dylan Hand <dhand@galois.com>
2019-04-11 12:29:56 -0700

    Update init_submodule script to include new boom repos and handle changes to repo URLs

--- init_submodules.sh
+++ init_submodules.sh
@@ -4,12 +4,22 @@ home=$(pwd)
 
 # Initialize the minimum number of submodules necessary to build the project
 # This reduces the runtime of git status and other git commands
+git submodule sync
+
 git submodule update --init --recursive riscv-tools \
 FreeRTOS-mirror bluespec-processors/P1/Piccolo  bluespec-processors/P2/Flute busybox riscv-linux bluespec-processors/P3/Tuba
 git submodule update --init chisel_processors
 
 cd chisel_processors
+git submodule sync
 git submodule update --init rocket-chip
 git submodule update --init P3/boom-template
+cd P3/boom-template
+git submodule sync
+git submodule update --init boom rocket-chip
 cd rocket-chip
+git submodule sync
+git submodule update --init firrtl chisel3 hardfloat
+cd ../../../rocket-chip
+git submodule sync
 git submodule update --init firrtl chisel3 hardfloat
