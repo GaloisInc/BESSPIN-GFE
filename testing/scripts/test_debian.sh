9fc412eb7995d7d95d02eec58ab2d4d9857a5781
Max Waugaman <mwaugaman@galois.com>
2019-04-15 11:49:37 -0700

    Added shell script for testing debian

---
+++ testing/scripts/test_debian.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Get the path to the script folder of the git repository
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+source $SCRIPT_DIR/../../setup_env.sh
+err_msg $SETUP_ENV_ERR "Sourcing setup_env.sh failed"
+
+linux_folder=$BASE_DIR/bootmem/
+python_folder=$BASE_DIR/bootmem/
+python_unittest_script=test_gfe_unittest.py
+
+function linux_test {
+	cd $linux_folder
+	make
+	err_msg $? "Building Linux failed"
+
+	cd $BASE_DIR/testing/scripts
+	python $python_unittest_script TestLinux.$1
+	err_msg $? "One or more Linux Tests failed"
+}
+
+linux_test test_busybox_boot
+
+
