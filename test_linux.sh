450f0d25e9396c70b2e9e42c6be6dbde11c91966
Max Waugaman <mwaugaman@galois.com>
2019-04-15 14:25:57 -0700

    combined debian test script into the linux test script

--- test_linux.sh
+++ test_linux.sh
@@ -9,16 +9,45 @@ cd $BASE_DIR/testing/scripts
 linux_folder=$BASE_DIR/bootmem/
 python_unittest_script=test_gfe_unittest.py
 
+function proc_linux_usage {
+    echo "Usage: $0 [busybox|debian]"
+    echo "Please specify busybox or debian!"
+}
+
+function linux_picker {
+	# Parse the processor selection
+	if [ "$1" == "debian" ]; then
+	        linux_image="debian"
+	elif [ "$1" == "busybox" ]; then
+	        linux_image="busybox"
+	else
+        proc_linux_usage
+        exit -1
+	fi
+}
+
 function linux_test {
 	cd $linux_folder
-	make
+	if [ "$linux_image" == "debian" ]; then
+		make debian
+	else
+		make
+	fi
 	err_msg $? "Building Linux failed"
+
+	if [ "$linux_image" == "debian" ]; then
+		# PROGRAM FPGA FLASH
+		cd $BASE_DIR
+		$BASE_DIR/tcl/program_flash datafile bootmem/bootmem.bin
+		err_msg $? "Programming flash failed"
+	fi
 
 	cd $BASE_DIR/testing/scripts
 	python $python_unittest_script TestLinux.$1
 	err_msg $? "One or more Linux Tests failed"
 }
 
-linux_test test_boot
+linux_picker $1
+linux_test test_busybox_boot
 
 
