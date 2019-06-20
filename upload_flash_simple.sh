ed0b2bbbfffc12e90b7019481587cffa4ddbecb3
Michal Podhradsky <mpodhradsky@galois.com>
2019-06-20 15:37:07 -0700

    Better flash upload script

--- upload_flash_simple.sh
+++ upload_flash_simple.sh
@@ -5,18 +5,37 @@
 
 if (( $# != 2 )); then
     echo "Illegal number of parameters"
-    echo "Usage: $0 path-to-bitfile path-to-elf"
+    echo "Usage: $0 processor_name path-to-elf"
     exit -1
 fi
+
+# Get the path to the script folder of the git repository
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source $BASE_DIR/setup_env.sh
+err_msg $SETUP_ENV_ERR "Sourcing setup_env.sh failed"
 
 prog=`basename $2`
 echo "Copying " $prog
 
+# Make a flash-compatible binary
 cp $2 bootmem/.
 cd bootmem
 PROG=$prog make -f Makefile.freertos
+rm bootmem/$prog
+cd -
 
-echo "Uploading..."
-tcl/program_flash bitfile $1
+if [ "$proc_name" == "chisel_p1" ] ; then 
+    bitfile_path=bitstreams/soc_chisel_p1.bit
+elif [ "$proc_name" == "bluespec_p1" ] ; then 
+    bitfile_path=bitfile bitstreams/soc_bluespec_p1.bit
+else
+    echo "Unsupported processor: " $proc_name
+    exit -1
+fi
+
+echo "Uploading bitstream"
+tcl/program_flash bitfile $bitfile_path
+echo "Bitstream upload finished"
+
 tcl/program_flash datafile bootmem/bootmem.bin
 echo "Done! Power cycle your FPGA."
