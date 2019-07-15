109c8057116bcbed0b19a017953a4bf8b3164870
Ramy Tadros <rtadros@galois.com>
2019-07-15 13:55:49 -0700

    Finalizing busbox + starting debian

--- runOnLinux/runOnLinux.sh
+++ runOnLinux/runOnLinux.sh
@@ -9,23 +9,26 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 err_msg $SETUP_ENV_ERR "Sourcing setup_env.sh failed"
 
-if [ "$1" == "--help" ] || [ $# -lt 3 ] ; then runOnLinux_usage; exit; fi
+runOnLinux_args "$@"
 
-proc_picker $1
-if [ ${proc_name: -1} -eq 1 ]; then xlen_picker 32; else xlen_picker 64; fi
-linux_picker $2
-check_file $3 "$0: Error: $3 is not found."
-checkMode $4
+if [ ${proc_name: -1} -eq 1 ]; then 
+    xlen_picker 32
+    err_msg 1 "$0: NotImplementedError: runOnLinux is not implemented for P1 processors."
+else 
+    xlen_picker 64
+fi
 
-#### Cross compiling the user C file
-echo "$0: Cross compiling $3.."
-make clean 
-make PROG=$3 XLEN=$XLEN
-err_msg $? "$0: Cross compiling $3 failed"
+if [ $isProg -eq 1 ]; then
+    #### Cross compiling the user C file
+    echo "$0: Cross compiling $progToRun.."
+    make clean 
+    make PROG=$progToRun XLEN=$XLEN
+    err_msg $? "$0: Cross compiling $progToRun failed"
+fi
 
 #### Building linux and programming the FPGA
-if [ $doSkipFPGA -ne 1 ]; then
-    echo "$0: Building the linux [$2] image.."
+if [ $isFastForward -ne 1 ]; then
+    echo "$0: Building the linux [$linux_image] image.."
     linux_folder=$BASE_DIR/../bootmem/
     cd $linux_folder
     if [ "$linux_image" == "debian" ]; then
@@ -41,15 +44,19 @@ if [ $doSkipFPGA -ne 1 ]; then
     err_msg $? "$0: Programming the FPGA failed"
     sleep 1
 else
-    echo "$0: SkipFPGA mode is activated."
+    echo "$0: FastForward mode is activated."
     echo "$0: Assuming linux is up on the FPGA."
 fi
 
 #### Booting linux on the FPGA
 echo "$0: Booting $linux_image on the FPGA.."
 cd $BASE_DIR/../testing/scripts/
-#python test_gfe_unittest.py TestLinux.test_${linux_image}_terminal
-echo "python test_runOnLinux.py TestRunOnLinux.test_${linux_image}_terminal"
-python runOnLinux.py RunOnLinux.test_${linux_image}_terminal
-err_msg $? "$0: Testing the program failed."
-echo "$0: The program ran successfully."
+if [ $isDebug -ne 1 ]; then
+    python runOnLinux.py RunOnLinux.test_${linux_image}_runAprog
+elif [ $isProg -ne 1 ] ; then
+    python runOnLinux.py RunOnLinux.test_${linux_image}_terminal
+else
+    python runOnLinux.py RunOnLinux.test_${linux_image}_runANDterminal
+fi
+err_msg $? "$0: Running on Linux failed."
+echo "$0: Running on Linux ran successfully."
