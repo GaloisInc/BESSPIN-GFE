33835b1f075397b5aad97a4c795abb6f113b692c
Max Orhai <max.orhai@galois.com>
2019-10-03 11:08:56 -0700

    Only clone riscv-gnu-toolchain as needed.

--- install/build-toolchain.sh
+++ install/build-toolchain.sh
@@ -11,12 +11,17 @@ rm -rf /opt/riscv/
 # System packages needed for the build:
 apt-get install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
 
-git clone --recursive https://github.com/riscv/riscv-gnu-toolchain
-cd riscv-gnu-toolchain
+if [ -d riscv-gnu-toolchain ]; then
+    git clone https://github.com/riscv/riscv-gnu-toolchain
+fi
+pushd riscv-gnu-toolchain
+make distclean
 # Snapshot of master on 2019-10-1 -- update as needed
 git checkout 2855d82
+git submodule update --init --recursive
 source configure --prefix /opt/riscv --enable-multilib
 make linux
 make
+popd
 
 tar -czf riscv-gnu-toolchains.tar.gz /opt/riscv
