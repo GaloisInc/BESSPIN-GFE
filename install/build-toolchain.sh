1a0be28c976dc623ffc19bca4948ea3be4b3a1f9
Max Orhai <max.orhai@galois.com>
2019-10-14 10:57:01 -0700

    Build gnu toolchains configured with cmodel=medany.

--- install/build-toolchain.sh
+++ install/build-toolchain.sh
@@ -4,9 +4,10 @@
 
 set -eux
 
-# WARNING: it wipes out any existing /opt/riscv directory,
-# so you may want to back that up before running this!
-rm -rf /opt/riscv/
+# Avoid overwriting any existing /opt/riscv directory
+if [ -d /opt/riscv ]; then
+    mv /opt/riscv /opt/riscv.old
+fi
 
 # System packages needed for the build:
 apt-get install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
@@ -14,14 +15,16 @@ apt-get install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev l
 if [ ! -d riscv-gnu-toolchain ]; then
     git clone https://github.com/riscv/riscv-gnu-toolchain
 fi
-pushd riscv-gnu-toolchain
-make distclean
-# Snapshot of master on 2019-10-1 -- update as needed
-git checkout 2855d82
+cd riscv-gnu-toolchain
+git clean -f
+rm -f Makefile
+git pull
+# Snapshot of master on 2019-10-10 -- update as needed
+git checkout d5bea51083ec38172b84b7cd5ee99bfcb8d2e7b0
 git submodule update --init --recursive
-source configure --prefix /opt/riscv --enable-multilib
+./configure --prefix /opt/riscv --enable-multilib --with-cmodel=medany
 make linux
 make
-popd
+cd ..
 
 tar -czf riscv-gnu-toolchains.tar.gz /opt/riscv
