095a6c5b94f94ff272768e87b12a7f794653de97
Michal Podhradsky <mpodhradsky@galois.com>
2019-11-14 09:11:45 -0800

    Update kernel configs

--- debian/build_stage1_initramfs.sh
+++ debian/build_stage1_initramfs.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 chroot_dir="riscv64-chroot"
 
 ./create_chroot.sh stage0
-./create_chroot.sh stage1
+./create_chroot.sh stage1 # tar this as_fake_root ?
 
 ./create_chroot.sh as_fake_root cp stage1-init "$chroot_dir/init"
 
@@ -18,4 +18,4 @@ rm "$chroot_dir/debootstrap/debootstrap.old"
 cd "$chroot_dir"
 find . -print0 | \
     ../create_chroot.sh as_fake_root cpio --null --create --format=newc | \
-    gzip --best >../stage1-initramfs.cpio.gz
+    xz -c --best >../stage1-initramfs.cpio.xz
--- debian/setup_scripts/install_sysvinit.sh
+++ debian/setup_scripts/install_sysvinit.sh
@@ -35,34 +35,3 @@ po::powerokwait:/etc/init.d/powerfail stop
 
 T0:2345:respawn:/sbin/getty -L console 115200 vt100
 " > /etc/inittab
-
-# Use sysvinit to provide /init
-ln -sf /sbin/init /init
-
-echo "
-id:3:initdefault:
-
-si::sysinit:/etc/init.d/rcS
-
-~:S:wait:/sbin/sulogin --force
-
-
-l0:0:wait:/etc/init.d/rc 0
-l1:1:wait:/etc/init.d/rc 1
-l2:2:wait:/etc/init.d/rc 2
-l3:3:wait:/etc/init.d/rc 3
-l4:4:wait:/etc/init.d/rc 4
-l5:5:wait:/etc/init.d/rc 5
-l6:6:wait:/etc/init.d/rc 6
-z6:6:respawn:/sbin/sulogin --force
-
-ca:12345:ctrlaltdel:/sbin/shutdown -t1 -a -r now
-
-
-pf::powerwait:/etc/init.d/powerfail start
-pn::powerfailnow:/etc/init.d/powerfail now
-po::powerokwait:/etc/init.d/powerfail stop
-
-
-T0:2345:respawn:/sbin/getty -L console 115200 vt100
-" > /etc/inittab
