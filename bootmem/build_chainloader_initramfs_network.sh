a6b7908d224280c381130a49c3ad9a2f14dad11c
Michal Podhradsky <mpodhradsky@galois.com>
2019-11-18 15:32:56 -0800

    Add network chainloader init

---
+++ bootmem/build_chainloader_initramfs_network.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")"
+
+die() {
+    echo "$@" 1>&2
+    exit 1
+}
+
+[ -z "$BUSYBOX_PREFIX" ] && die "must set \$BUSYBOX_PREFIX (busybox _install dir)"
+: ${SOURCE_DATE_EPOCH:=$(date +%s)}
+
+cat >chainloader-initramfs.files <<EOF
+file /init chainloader-init-network 0755 0 0
+EOF
+
+# Busybox components, all squashed to owner 0:0
+"$CPIO_UTILS_PREFIX"gen_initramfs_list.sh -u squash -g squash $BUSYBOX_PREFIX >>chainloader-initramfs.files
+# Default minimum file list
+"$CPIO_UTILS_PREFIX"gen_initramfs_list.sh -d >>chainloader-initramfs.files
+
+"$CPIO_UTILS_PREFIX"gen_init_cpio -t $SOURCE_DATE_EPOCH chainloader-initramfs.files | \
+    gzip --best >chainloader-initramfs.cpio.gz
