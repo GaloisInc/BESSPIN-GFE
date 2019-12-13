7113bc233077c8377f4ce4b9685de4d0062e204b
James Clarke <jrtc27@jrtc27.com>
2019-12-13 15:37:04 +0000

    init_submodules.sh: Don't initialise unwanted chisel implementations

--- init_submodules.sh
+++ init_submodules.sh
@@ -15,18 +15,3 @@ git submodule update --init tool-suite FreeRTOS-mirror busybox newlib
 git submodule update --init --recursive \
 bluespec-processors/P1/Piccolo bluespec-processors/P2/Flute \
 riscv-linux bluespec-processors/P3/Tuba riscv-tests riscv-pk
-git submodule update --init chisel_processors
-
-cd chisel_processors
-git submodule sync
-git submodule update --init rocket-chip
-git submodule update --init P3/boom-template
-cd P3/boom-template
-git submodule sync
-git submodule update --init boom rocket-chip
-cd rocket-chip
-git submodule sync
-git submodule update --init firrtl chisel3 hardfloat
-cd ../../../rocket-chip
-git submodule sync
-git submodule update --init firrtl chisel3 hardfloat
