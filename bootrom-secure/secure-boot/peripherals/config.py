87b655589ec581a5774602a1eb9abe002bd08a76
Daniel M. Zimmerman <dmz@galois.com>
2020-01-03 14:06:59 -0800

    Updated the device tree for current GFE.

--- bootrom-secure/secure-boot/peripherals/config.py
+++ bootrom-secure/secure-boot/peripherals/config.py
@@ -5,15 +5,15 @@ from uart import UART
 from dram import DRAM
 from flash_os import Flash_OS
 
-RAM_DEVICE = DRAM(base=0x80000000, size=2 * 1024 * 1024 * 1024)
+RAM_DEVICE = DRAM(base=0xC0000000, size=0x40000000)
 DEVICES = [
     Ethernet(base=0x62100000, size=256 * 1024),
     UART(base=0x62300000, size=4 * 1024),
-    Flash_OS(flash_base=0x44000100, ram_base=0x84000000, size=541028,
+    Flash_OS(flash_base=0x44000100, ram_base=0xC4000000, size=541028,
              sha256sum=0xfbc49333ec87ed7a78482f16667a13c32756ed5177b2cbdd1bc4acf7285f036d,
              ram_device=RAM_DEVICE)
 ]
-BOOT_ADDRESS=0x84000000
+BOOT_ADDRESS=0xC4000000
 
 if __name__ == '__main__':
     peripherals_config(DEVICES, BOOT_ADDRESS)
