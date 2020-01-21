13ad79418ec2dd826cdbe30272585d3b852342e6
Daniel M. Zimmerman <dmz@galois.com>
2020-01-21 10:28:47 -0800

    Updated to move the boot loader's stack higher in memory.

--- bootrom-secure/secure-boot/peripherals/config.py
+++ bootrom-secure/secure-boot/peripherals/config.py
@@ -9,11 +9,11 @@ RAM_DEVICE = DRAM(base=0xC0000000, size=0x40000000)
 DEVICES = [
     Ethernet(base=0x62100000, size=256 * 1024),
     UART(base=0x62300000, size=4 * 1024),
-    Flash_OS(flash_base=0x44000100, ram_base=0xC4000000, size=541028,
-             sha256sum=0xfbc49333ec87ed7a78482f16667a13c32756ed5177b2cbdd1bc4acf7285f036d,
+    Flash_OS(flash_base=0x44000100, ram_base=0xC0000000, size=537356,
+             sha256sum=0xa110cdd552e7388ee39c24a0ff5cb2b6f4cd15e9c1ebc317100fbcfe982138fb,
              ram_device=RAM_DEVICE)
 ]
-BOOT_ADDRESS=0xC4000000
+BOOT_ADDRESS=0xC0000000
 
 if __name__ == '__main__':
     peripherals_config(DEVICES, BOOT_ADDRESS)
--- bootrom-secure/secure-boot/peripherals/flash_os.py
+++ bootrom-secure/secure-boot/peripherals/flash_os.py
@@ -3,7 +3,6 @@ from dram import STACK_SIZE
 
 def Flash_OS(flash_base, ram_base, size, sha256sum, ram_device):
     d = Device('Flash-based OS', flash_base, size, ram_device)
-    assert(ram_base >= ram_device.start_addr + STACK_SIZE)
     d.copy(ram_base, flash_base, size)
     d.fence()
     d.sha256(ram_base, size, sha256sum)
