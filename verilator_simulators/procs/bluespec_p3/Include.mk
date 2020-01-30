e9e3d3834e94065bda61998b69d8c1822cdf4b9b
Niraj Sharma <niraj.sharma@bluespec.com>
2020-01-30 14:12:54 -0500

    Changed path for core source files for bluespec_p3

--- verilator_simulators/procs/bluespec_p3/Include.mk
+++ verilator_simulators/procs/bluespec_p3/Include.mk
@@ -3,5 +3,5 @@
 XLEN      = 64
 ISA       = ACDFIMSU
 PROCESSOR = Toooba
-PROCESSOR_RTL = $(REPO)/bluespec-processors/P3/$(PROCESSOR)/src_SSITH_P3/Verilog_RTL
+PROCESSOR_RTL = $(REPO)/bluespec-processors/P3/$(PROCESSOR)/src_SSITH_P3_sim/Verilog_RTL
 TOPNAME   = mkP3_Core
