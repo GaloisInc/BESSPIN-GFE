0800ef813d455bc5fabb6a630153dd44d5230920
Joe Stoy <stoy@bluespec.com>
2020-01-31 16:05:13 -0500

    Add TV to verilator sims; add an SVF physical constraint; revert program_fpga.sh

--- verilator_simulators/Verilog_RTL/mkUART.v
+++ verilator_simulators/Verilog_RTL/mkUART.v
@@ -811,7 +811,7 @@ module mkUART(CLK,
 
   // register rg_state
   assign rg_state$D_IN = 1'd1 ;
-  assign rg_state$EN = WILL_FIRE_RL_rl_reset ;
+  assign rg_state$EN = f_reset_reqs$EMPTY_N && f_reset_rsps$FULL_N ;
 
   // register rg_thr
   assign rg_thr$D_IN = MUX_rg_dll$write_1__VAL_2 ;
@@ -827,11 +827,11 @@ module mkUART(CLK,
 
   // submodule f_reset_reqs
   assign f_reset_reqs$ENQ = EN_server_reset_request_put ;
-  assign f_reset_reqs$DEQ = WILL_FIRE_RL_rl_reset ;
+  assign f_reset_reqs$DEQ = f_reset_reqs$EMPTY_N && f_reset_rsps$FULL_N ;
   assign f_reset_reqs$CLR = 1'b0 ;
 
   // submodule f_reset_rsps
-  assign f_reset_rsps$ENQ = WILL_FIRE_RL_rl_reset ;
+  assign f_reset_rsps$ENQ = f_reset_reqs$EMPTY_N && f_reset_rsps$FULL_N ;
   assign f_reset_rsps$DEQ = EN_server_reset_response_get ;
   assign f_reset_rsps$CLR = 1'b0 ;
 
