fe1c56a52c30e38701d350105cd23454cfefd411
Joe Stoy <stoy@bluespec.com>
2020-02-11 17:31:49 -0500

    revised README.md, scripts to implement new bitstream naming scheme

--- setup_env.sh
+++ setup_env.sh
@@ -34,7 +34,7 @@ function check_file {
 }
 
 function proc_usage {
-    echo "Usage: $0 [chisel_p1|chisel_p2|chisel_p3|bluespec_p1|bluespec_p2|bluespec_p3]"
+    echo "Usage: $0 [chisel_p1|chisel_p2|chisel_p2_pcie|chisel_p3|bluespec_p1|bluespec_p2|bluespec_p2_pcie|bluespec_p3]"
     echo "Please specify a bluespec or chisel processor!"
 }
 
@@ -44,12 +44,16 @@ function proc_picker {
 	        proc_name="bluespec_p1"
 	elif [ "$1" == "bluespec_p2" ]; then
 	        proc_name="bluespec_p2"
+	elif [ "$1" == "bluespec_p2_pcie" ]; then
+	        proc_name="bluespec_p2_pcie"
 	elif [ "$1" == "bluespec_p3" ]; then
 	        proc_name="bluespec_p3"
 	elif [ "$1" == "chisel_p1" ]; then
 	        proc_name="chisel_p1"
 	elif [ "$1" == "chisel_p2" ]; then
 	        proc_name="chisel_p2"
+	elif [ "$1" == "chisel_p2_pcie" ]; then
+	        proc_name="chisel_p2_pcie"
 	elif [ "$1" == "chisel_p3" ]; then
 	        proc_name="chisel_p3"
 	else
