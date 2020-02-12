c093a780f1f1bfc49c6bf54342e9e5642623c1f0
Daniel M. Zimmerman <dmz@galois.com>
2020-02-12 14:51:54 -0800

    Initial attempt at automating secure bootrom generation.

--- setup_soc_project.sh
+++ setup_soc_project.sh
@@ -24,15 +24,43 @@ case "$proc_name" in
 	echo "use_xdma is valid only for P2 processors"
 	;;
     esac
+    
+# Set up the bootrom directory
+if [ -d "$BASE_DIR/bootrom-configured" ]; then
+    echo "moving old configured bootrom"
+    msec=`date +%s`
+    mv "$BASE_DIR/bootrom-configured" "$BASE_DIR/bootrom-configured.$msec"
+fi
+
+if [ -z "$2" ]; then
+    # if there isn't a second parameter, use the default bootrom
+    echo "generating default bootrom"
+    cp -pr "$BASE_DIR/bootrom" "$BASE_DIR/bootrom-configured"
+elif [ -f "$2" ]; then
+    # if the second parameter points to an existing file, measure it and
+    # configure the bootrom
+    sha=`sha256sum $2 | awk '{print $1}'`
+    len=`ls -nl $2 | awk '{print $5}'`
+    echo "generating secure bootrom for binary '$2'"
+    echo "       length: $len"
+    echo "    sha256sum: $sha"
+    cp -pr "$BASE_DIR/bootrom-secure" "$BASE_DIR/bootrom-configured"
+    $BASE_DIR/bootrom-configured/configure.sh $sha $len
+    pwd
+    err_msg $? "failed to configure the secure bootrom"
+else
+    proc_usage
+    err_msg 1 "specified secure boot binary image doesn't exist"
+fi
 
 # Compile the bootrom and set the clock frequency
-cd $BASE_DIR/bootrom
+cd $BASE_DIR/bootrom-configured
 case "$proc_name" in
     *p1)
 	make --always-make XLEN=32 CROSS_COMPILE=riscv64-unknown-elf- CPU_SPEED=50000000 NO_PCI=$no_xdma
 	clock_freq_mhz=50
 	;;
-    *p2)
+    *p2*)
 	make --always-make XLEN=64 CROSS_COMPILE=riscv64-unknown-elf- CPU_SPEED=100000000 NO_PCI=$no_xdma
 	clock_freq_mhz=100
 	;;
