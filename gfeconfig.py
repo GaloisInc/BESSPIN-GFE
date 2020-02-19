cbf4793d6c74e8c3dc263b7e7757158ea845f579
Michal Podhradsky <mpodhradsky@galois.com>
2020-02-19 14:32:45 -0800

    [FreeBSD] Increase timeout

--- gfeconfig.py
+++ gfeconfig.py
@@ -89,7 +89,7 @@ class Config(object):
 
         absent_contents = {'boot': None}
 
-        timeouts = {'boot': 300} # large timeout to account for loading the binary over JTAG
+        timeouts = {'boot': 600} # large timeout to account for loading the binary over JTAG
 
         self.freebsd_expected_contents = expected_contents
         self.freebsd_absent_contents = absent_contents
