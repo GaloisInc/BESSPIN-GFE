8437909aa82843ae8f932c349f28199d932d4831
Steven Osborn <steven@lolsborn.com>
2020-02-25 10:17:47 -0800

    missing build files for helloworld, gitignore generated files

--- aws_helloworld/build/scripts/encrypt.tcl
+++ aws_helloworld/build/scripts/encrypt.tcl
@@ -34,7 +34,7 @@ if {[llength [glob -nocomplain -dir $TARGET_DIR *]] != 0} {
 file copy -force $CL_DIR/design/cl_hello_world_defines.vh             $TARGET_DIR
 file copy -force $CL_DIR/design/cl_id_defines.vh                      $TARGET_DIR
 file copy -force $CL_DIR/design/cl_hello_world.sv                     $TARGET_DIR 
-file copy -force $CL_DIR/../common/design/cl_common_defines.vh        $TARGET_DIR 
+file copy -force $CL_DIR/design/cl_common_defines.vh                  $TARGET_DIR 
 file copy -force $UNUSED_TEMPLATES_DIR/unused_apppf_irq_template.inc  $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_cl_sda_template.inc     $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_ddr_a_b_d_template.inc  $TARGET_DIR
