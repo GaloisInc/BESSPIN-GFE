da8adc385d25adec34d0e2ba4ae7e178e4a8ebe9
Michal Podhradsky <mpodhradsky@galois.com>
2020-03-05 13:27:46 -0800

    Add docker to the dependencies

--- install/deps.sh
+++ install/deps.sh
@@ -42,3 +42,20 @@ apt install -y libglib2.0-dev libpixman-1-dev libarchive-dev bsdtar libbz2-dev
 # Install and configure TFTP server
 apt install -y atftpd
 chmod 777 /srv/tftp
+
+# Install docker engine
+apt-get remove -y docker docker-engine docker.io containerd runc
+apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
