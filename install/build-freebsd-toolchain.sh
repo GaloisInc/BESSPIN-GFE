0276aa98fe15b5e5ba8b4ad28b12de45867e0b23
Lyell Read <lyellread@gmail.com>
2020-03-27 06:04:37 +0000

    Moving to HTTPS clone, cleaning up script

--- install/build-freebsd-toolchain.sh
+++ install/build-freebsd-toolchain.sh
@@ -6,25 +6,23 @@ set -eux
 
 # Avoid overwriting any existing /opt/riscv directory
 if [ -d /opt/riscv ]; then
-    mv /opt/riscv /opt/riscv.old
+    mv -f /opt/riscv /opt/riscv.old
 fi
 
 # System packages needed for the build:
 apt-get install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
 
 if [ ! -d riscv-gnu-toolchain-freebsd ]; then
-    git clone git@github.com:freebsd-riscv/riscv-gnu-toolchain.git riscv-gnu-toolchain-freebsd
+    git clone https://github.com/riscv/riscv-gnu-toolchain.git riscv-gnu-toolchain-freebsd
 fi
 
 cd riscv-gnu-toolchain-freebsd
 git checkout master
 git clean -f
-make clean
-rm -f Makefile
 git pull
 
 # Snapshot of master on 2019-10-10 -- update as needed
-git checkout 1505830a3b757b3e65c15147388dd1a91ee2c786
+# git checkout 1505830a3b757b3e65c15147388dd1a91ee2c786
 git submodule update --init --recursive
 ./configure --prefix /opt/riscv
 make freebsd
