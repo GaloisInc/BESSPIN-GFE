e9e847c0010ca5390bba5058d670e3a9bff6fc76
Lyell Read <lyellread@gmail.com>
2020-03-27 20:29:23 +0000

    Adding output style and iteration progress

--- pytest_consistency.py
+++ pytest_consistency.py
@@ -13,16 +13,19 @@ if __name__ == "__main__":
 
     failures = 0
 
-    print ("Running", ' '.join(subprocess_command), iterations, "times. . .")
+    print (" ==> Running", ' '.join(subprocess_command), iterations, "times. . .")
 
     for x in range(0, iterations):
+        print(" ==> Run [", x, "/", iterations, "] Started." 
         result = run_test(subprocess_command)
         if result != 0:
             # Exited failure
-            print ("Run", x, "failed with code", result)
+            print (" ==> Run", x, "failed with code", result)
             failures += 1
+Â       print(" ==> Run [", x, "/", iterations, "] Finished." 
 
-    print("Completed", iterations, "runs of", ' '.join(subprocess_command), "results:")
-    print("Consistency:", failures, "failures during", iterations, "iterations\n",
+
+    print(" ==> Completed", iterations, "runs of", ' '.join(subprocess_command), "results:")
+    print(" ==> Consistency:", failures, "failures during", iterations, "iterations\n",
             " ==> [", failures/iterations*100, "% Failures]", "\n",
             " ==> [", 100-(failures/iterations*100), "% Success]")
