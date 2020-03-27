e452a8f86e240884cc61332ba5c666afa4254f7a
Lyell Read <lyellread@gmail.com>
2020-03-27 07:25:02 +0000

    Finalizing first draft of pytest_consistency.py

--- pytest_consistency.py
+++ pytest_consistency.py
@@ -2,18 +2,18 @@ import sys
 import subprocess
 
 def run_test (command):
-    test = subprocess.Popen(command, stdout=subprocess.PIPE)
-    return test.returncode
+    test = subprocess.call(command)
+    return test
 
 if __name__ == "__main__":
     pytest = "./pytest_processor.py"
     subprocess_command = [pytest]
-    [subprocess_command.append(x) for x in sysargv[2:]]
-    iterations = sys.argv[1]
+    [subprocess_command.append(x) for x in sys.argv[2:]]
+    iterations = int(sys.argv[1])
 
     failures = 0
 
-    print ("Running", ' '.join(subrocess_command), iterations, "times. . .")
+    print ("Running", ' '.join(subprocess_command), iterations, "times. . .")
 
     for x in range(0, iterations):
         result = run_test(subprocess_command)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             print ("Run", x, "failed with code", result)
             failures += 1
 
-    print("Completed", iterations, "runs")
-    print("Consistency:", failures, "/", iterations, "\n", 
-            " ==> [", failures/iterations, "% Fail]", "\n",
-            " ==> [", 100-(failures/iterations), "% Success]")
+    print("Completed", iterations, "runs of", ' '.join(subprocess_command), "results:")
+    print("Consistency:", failures, "failures during", iterations, "iterations\n",
+            " ==> [", failures/iterations*100, "% Failures]", "\n",
+            " ==> [", 100-(failures/iterations*100), "% Success]")
