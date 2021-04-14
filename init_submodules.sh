ae8fdb6c0132d553164d4c13b87ba7d40d9c31ed
rtadros <rtadros@galois.com>
2021-04-14 15:50:49 -0700

    update the init_submodules script following the new moves

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-home=$(pwd)
+set -ex
 
 # Initialize the minimum number of submodules necessary to build the project
 # This reduces the runtime of git status and other git commands
@@ -9,36 +9,28 @@ git submodule sync
 git submodule update --init benchmarks/coremark
 git submodule update --init benchmarks/mibench2
 git submodule update --init riscv-openocd
-
-git submodule update --init riscv-tests
-cd riscv-tests
-git submodule sync
-cd ..
-
 git submodule update --init FreeRTOS-mirror
 git submodule update --init busybox
-git submodule update --init --recursive \
-bluespec-processors/P1/Piccolo
-git submodule update --init --recursive \
-bluespec-processors/P2/Flute
-git submodule update --init --recursive \
-bluespec-processors/P3/Toooba
-git submodule update --init --recursive \
-riscv-linux
-git submodule update --init --recursive \
-riscv-tests
-git submodule update --init --recursive \
-riscv-pk
-git submodule update --init chisel_processors
 git submodule update --init freebsd/cheribsd
 
+git submodule update --init --recursive \
+    riscv-tests
+git submodule update --init --recursive \
+    bluespec-processors/P1/Piccolo
+git submodule update --init --recursive \
+    bluespec-processors/P2/Flute
+git submodule update --init --recursive \
+    bluespec-processors/P3/Toooba
+git submodule update --init --recursive \
+    riscv-linux
+git submodule update --init --recursive \
+    riscv-pk
 
-cd chisel_processors
-git submodule sync
-git submodule update --init rocket-chip
-git submodule update --init chipyard
-cd rocket-chip
-git submodule sync
-git submodule update --init firrtl chisel3 hardfloat
+# chisel_processors has a co-dependency loop (between ucb-bar/chipyar and firesim/firesim)
+git submodule update --init chisel_processors
+git -C chisel_processors submodule sync
+git -C chisel_processors submodule update --init rocket-chip
+git -C chisel_processors submodule update --init chipyard
+git -C chisel_processors/rocket-chip submodule sync
+git -C chisel_processors/rocket-chip submodule update --init firrtl chisel3 hardfloat
 
-cd $home
